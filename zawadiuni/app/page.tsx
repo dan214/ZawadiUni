114298947b207f0b1d2e1479c982bf7159e1c6f8
Daniel Irungu <v-dairungu@microsoft.com>
2024-01-15 17:30:13 +0300

    add tables with dummy data

--- zawadiuni/app/page.tsx
+++ zawadiuni/app/page.tsx
@@ -1,83 +1,311 @@
 import React, { useState } from 'react';
-import { Col, Row } from 'reactstrap';
-import DataCard from '../components/Charts/DataCard';
-import StabilityPieChart from '../components/Charts/DataCard/PieChart';
-import StabilityLineChart from '../components/Charts/DataCard/BarChart';
-import axiosApi from '@/helpers/axios';
+import { Row } from 'react-bootstrap';
+import { Button, Card, CardText, CardTitle, Col, Table } from 'reactstrap';
+
 
 export default async function Home() {
-
-  const courses = await getData()
-
   return (
-    <div className="stability-report">
-      <Row className="mb-4 mt-4">
-        <Col xl={3} lg={12} md={12} sm={12} xs={24}>
-          {/* <DataCard
-            cardTitle="Total Completed Tests"
-            cardValue={courses.toString()}
-            chartData={courses}
-            styles={{
-              cardBackground: '#0175e4',
-              fill: '#092453',
-            }}
-          /> */}
-        </Col>
-        <Col xl={3} lg={12} md={12} sm={12} xs={24}>
-          {/* <DataCard
-            cardTitle="Total User Errors"
-            cardValue={chartsData.totalUserErrors.toString()}
-            chartData={chartsData.totalUserErrorFailsChart}
-            styles={{
-              cardBackground: '#7066E0',
-              fill: '#4d0099',
-            }}
-          /> */}
-        </Col>
-        <Col xl={3} lg={12} md={12} sm={12} xs={24}>
-          {' '}
-          {/* <DataCard
-            cardTitle="Total Infra Fails"
-            cardValue={chartsData.totalInfraFails.toString()}
-            chartData={chartsData.totalInfraFailsChart}
-            styles={{
-              cardBackground: '#DC353D',
-              fill: 'red',
-            }}
-          /> */}
-        </Col>
-        <Col xl={3} lg={12} md={12} sm={12} xs={24}>
-          {/* <DataCard
-            cardTitle="Stability"
-            cardValue={chartsData.overallStability.toString() + ' %'}
-            chartData={chartsData.stabilityChart}
-            styles={{
-              cardBackground: '#027C02',
-              fill: '#158765',
-            }}
-          /> */}
-        </Col>
-      </Row>
-      <Row>
-        <Col xl={6} lg={12} md={12} sm={12} xs={24}>
-          {/* <StabilityPieChart data={chartsData.pieChartData} /> */}
-        </Col>
-        <Col xl={6} lg={12} md={12} sm={12} xs={24}>
-          {/* <StabilityLineChart data={chartsData.lineChartData} /> */}
-        </Col>
-      </Row>
+    <><Row>
+    </Row><div className="stability-report">
+        <Row><Col md={{
+          size: 5
+        }} sm={{
+          size: 5
+        }} lg={{
+          size: 6
+        }}>    <Card body>
+            <CardTitle tag="h1">
+              <b>Students</b>
+            </CardTitle>
+            <CardText>
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>
+                      #
+                    </th>
+                    <th>
+                      First Name
+                    </th>
+                    <th>
+                      Last Name
+                    </th>
+                    <th>
+                      Username
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">
+                      1
+                    </th>
+                    <td>
+                      Mark
+                    </td>
+                    <td>
+                      Otto
+                    </td>
+                    <td>
+                      @mdo
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      2
+                    </th>
+                    <td>
+                      Jacob
+                    </td>
+                    <td>
+                      Thornton
+                    </td>
+                    <td>
+                      @fat
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      3
+                    </th>
+                    <td>
+                      Larry
+                    </td>
+                    <td>
+                      the Bird
+                    </td>
+                    <td>
+                      @twitter
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </CardText>
+          </Card></Col><Col md={{
+            size: 5
+          }} sm={{
+            size: 5
+          }} lg={{
+            size: 6
+          }}>    <Card body>
+              <CardTitle tag="h1">
+                <b>Courses</b>
+              </CardTitle>
+              <CardText>
+                <Table striped>
+                  <thead>
+                    <tr>
+                      <th>
+                        #
+                      </th>
+                      <th>
+                        First Name
+                      </th>
+                      <th>
+                        Last Name
+                      </th>
+                      <th>
+                        Username
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">
+                        1
+                      </th>
+                      <td>
+                        Mark
+                      </td>
+                      <td>
+                        Otto
+                      </td>
+                      <td>
+                        @mdo
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">
+                        2
+                      </th>
+                      <td>
+                        Jacob
+                      </td>
+                      <td>
+                        Thornton
+                      </td>
+                      <td>
+                        @fat
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">
+                        3
+                      </th>
+                      <td>
+                        Larry
+                      </td>
+                      <td>
+                        the Bird
+                      </td>
+                      <td>
+                        @twitter
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </CardText>
+            </Card></Col></Row>
 
-
-    </div>
+        <Row><Col md={{
+          size: 5
+        }} sm={{
+          size: 5
+        }} lg={{
+          size: 6
+        }}>    <Card body>
+            <CardTitle tag="h1">
+              <b>Students</b>
+            </CardTitle>
+            <CardText>
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>
+                      #
+                    </th>
+                    <th>
+                      First Name
+                    </th>
+                    <th>
+                      Last Name
+                    </th>
+                    <th>
+                      Username
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">
+                      1
+                    </th>
+                    <td>
+                      Mark
+                    </td>
+                    <td>
+                      Otto
+                    </td>
+                    <td>
+                      @mdo
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      2
+                    </th>
+                    <td>
+                      Jacob
+                    </td>
+                    <td>
+                      Thornton
+                    </td>
+                    <td>
+                      @fat
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      3
+                    </th>
+                    <td>
+                      Larry
+                    </td>
+                    <td>
+                      the Bird
+                    </td>
+                    <td>
+                      @twitter
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </CardText>
+          </Card></Col><Col md={{
+            size: 5
+          }} sm={{
+            size: 5
+          }} lg={{
+            size: 6
+          }}>    <Card body>
+              <CardTitle tag="h1">
+                <b>Courses</b>
+              </CardTitle>
+              <CardText>
+                <Table striped>
+                  <thead>
+                    <tr>
+                      <th>
+                        #
+                      </th>
+                      <th>
+                        First Name
+                      </th>
+                      <th>
+                        Last Name
+                      </th>
+                      <th>
+                        Username
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">
+                        1
+                      </th>
+                      <td>
+                        Mark
+                      </td>
+                      <td>
+                        Otto
+                      </td>
+                      <td>
+                        @mdo
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">
+                        2
+                      </th>
+                      <td>
+                        Jacob
+                      </td>
+                      <td>
+                        Thornton
+                      </td>
+                      <td>
+                        @fat
+                      </td>
+                    </tr>
+                    <tr>
+                      <th scope="row">
+                        3
+                      </th>
+                      <td>
+                        Larry
+                      </td>
+                      <td>
+                        the Bird
+                      </td>
+                      <td>
+                        @twitter
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </CardText>
+            </Card></Col></Row>
+      </div></>
   )
-}
-
-
-async function getData() {
-
-  const suffixUrl = `/course`;
-  const courses = await axiosApi.getData(suffixUrl);
-  console.log("calling");
-  console.log(courses);
-  return courses;
 }
